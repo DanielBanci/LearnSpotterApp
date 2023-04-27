f92fc45ef10697971bbbfd86b250d9f680ac11bd
DanielBanci <ciprian.banci02@e-uvt.ro>
2023-04-27 12:33:36 +0300

    Commit3
    Deleted some garbage

--- src/app/App.java
+++ src/app/App.java
@@ -1,9 +1,23 @@
 package app;
 
+import app.ui_login.LoginWindow;
+
 import javax.swing.*;
 
+/**
+ * Main class, the launch of the app.
+ * Contains the frame of the app.
+ * @author Daniel
+ * @version 1.0
+ */
 public class App {
-    private JFrame mainFrame;
+    private static JFrame mainFrame;
 
-
+    public static void main(String[] args) {
+        mainFrame  = new JFrame();
+        mainFrame.setContentPane(new LoginWindow().contentPanel);
+        mainFrame.setBounds(0,0,1000,600);
+        mainFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        mainFrame.setVisible(true);
+    }
 }
